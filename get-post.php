d0cf269024b904f5ac815a8866c4e7828f670353
Dave <pelyuh.v@gmail.com>
2019-10-03 10:45:19 +0300

    user form upload file - fix

--- get-post.php
+++ get-post.php
@@ -16,7 +16,9 @@
         <input type="password" value="" id="user-password" name="user-password">
     </div>
     <div>
-        <button type="submit" name="user-form-send" value="user-form-send">Send</button>
+        <button type="submit" name="user-form-send" value="user-form-send">
+            Send
+        </button>
     </div>
 </form>
 
@@ -25,10 +27,13 @@
 <form method="post" action="" enctype="multipart/form-data">
     <div>
         <lable for="user-attachment-file">Attachment file</lable>
-        <input type="file" value="" id="user-attachment-file" name="user-attachment-file">
+        <input type="file" value="" id="user-attachment-file"
+               name="user-attachment-file">
     </div>
     <div>
-        <button type="submit" name="user-attachment-file" value="">Attachment file</button>
+        <button type="submit" name="user-attachment-file" value="">
+            Attachment file
+        </button>
     </div>
 </form>
 
@@ -36,29 +41,39 @@
 
 <?php
 
-echo 'Global array $_REQUEST:';
-echo "<pre>";
-print_r($_REQUEST);
-echo "</pre>";
+if (isset($_REQUEST['user-form-send'])) {
+  echo 'Global array $_REQUEST:';
+  echo "<pre>";
+  print_r($_REQUEST);
+  echo "</pre>";
+}
 
-echo 'Global array $_GET:';
-echo "<pre>";
-print_r($_GET);
-echo "</pre>";
+if (isset($_GET['user-form-send'])) {
+  echo 'Global array $_GET:';
+  echo "<pre>";
+  print_r($_GET);
+  echo "</pre>";
+}
 
-echo 'Global array $_POST:';
-echo "<pre>";
-print_r($_POST);
-echo "</pre>";
+if (isset($_POST['user-form-send'])) {
+  echo 'Global array $_POST:';
+  echo "<pre>";
+  print_r($_POST);
+  echo "</pre>";
+}
 
-echo 'Global array $_FILES:';
-echo "<pre>";
-print_r($_FILES);
-echo "</pre>";
+if (isset($_FILES['user-attachment-file'])) {
+  echo 'Global array $_FILES:';
+  echo "<pre>";
+  print_r($_FILES);
+  echo "</pre>";
 
-// dir - upload - chmod 777
-move_uploaded_file($_FILES['user-attachment-file']['tmp_name'], 'upload/' . $_FILES['user-attachment-file']['name']);
-
+  // dir - upload - chmod 777
+  move_uploaded_file(
+    $_FILES['user-attachment-file']['tmp_name'],
+    'upload/' . $_FILES['user-attachment-file']['name']
+  );
+}
 ?>
 
 </body>
