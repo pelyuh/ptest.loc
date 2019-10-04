83e099344abb3a8b7322756c4399607c672f6621
Dave <pelyuh.v@gmail.com>
2019-10-04 11:59:13 +0300

    first guest-book

---
+++ guest-book/index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>HTML</title>
+</head>
+<body>
+
+<section>
+    <div>
+        <form action="" method="post">
+            <lable for="guest-name">Guest Name</lable>
+            <br>
+            <input type="text" name="guest-name" id="guest-name" value="">
+            <br>
+            <lable for="guest-content">Content</lable>
+            <br>
+            <textarea name="guest-content" id="guest-content" cols="30" rows="10"></textarea>
+            <br>
+            <button type="submit" name="guest-content-send" value="guest-content-send">Send</button>
+        </form>
+    </div>
+</section>
+
+<?php
+
+if ( isset($_POST['guest-content-send']) ) {
+  $guest_content_item = $_POST['guest-name'] . "|" . $_POST['guest-content']  . "|" .  date('Y-m-d H:i:s') . "\n"  . "*-*";
+  file_put_contents( 'guest_content.txt', $guest_content_item, FILE_APPEND);
+}
+
+echo "<hr>";
+if ( file_exists('guest_content.txt') ) {
+  $guest_content_view = file_get_contents('guest_content.txt');
+  $guest_content_view3 = explode('*-*', $guest_content_view);
+
+  foreach ($guest_content_view3 as $value) {
+      if ($value != null) {
+        $str1 = explode('|', $value);
+
+        echo "<div>";
+        echo "<h3> Автор: " . $str1[0] . "</h3>";
+        echo "<p> Відгук: " . $str1[1] . "</p>";
+        echo "<p> Дата та час публікації: " . $str1[2] . "</p>";
+        echo "</div>";
+        echo "<br>";
+      }
+  }
+}
+
+?>
+
+
+</body>
+</html>
