d7494d9869588cae3e44125e04fcae39ff662a5c
Pelyukh Vitaliy <pelyuh.v@gmail.com>
2019-10-25 18:42:45 +0300

    error display enabled

--- oop.php
+++ oop.php
@@ -1,5 +1,8 @@
 <?php
 
+ini_set("display_errors", 1);
+error_reporting(E_ALL);
+
 require_once 'app/classes/classes.php';
 
 $product1 = new ShopProduct("Собачье сердце", "Булгаков", "Михаил", 5.99);
