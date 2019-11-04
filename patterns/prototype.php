9ac37456dd271d6a1c20929c73b83b93587e0a39
Pelyukh Vitaliy <pelyuh.v@gmail.com>
2019-11-04 17:24:59 +0200

    new file:   patterns/prototype.php

---
+++ patterns/prototype.php
@@ -0,0 +1,48 @@
+<?php
+
+class Sheep
+{
+    protected $name;
+    protected $category;
+
+    public function __construct(string $name, string $category = 'Горная овечка')
+    {
+        $this->name = $name;
+        $this->category = $category;
+    }
+
+    public function setName(string $name)
+    {
+        $this->name = $name;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function setCategory(string $category)
+    {
+        $this->category = $category;
+    }
+
+    public function getCategory()
+    {
+        return $this->category;
+    }
+}
+
+
+/*
+ * Затем он может быть клонирован следующим образом:
+ * */
+
+$original = new Sheep('Джолли');
+echo $original->getName(); // Джолли
+echo $original->getCategory(); // Горная овечка
+
+// Клонируем и модифицируем то что нужно
+$cloned = clone $original;
+$cloned->setName('Долли');
+echo $cloned->getName(); // Долли
+echo $cloned->getCategory(); // Горная овечка
